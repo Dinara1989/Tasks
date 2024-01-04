17b4dd625512ae65f3a42d54e224a298f52ffaae
Dinara <dinara198989@bk.ru>
2024-01-04 15:59:18 +0100

    microtasks, button, Handler

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 type ComponentType = {
-    students: Array<StudentType>
+    students: Array<StudentType> //один з варінтів
     // students: StudentType[] //інший варіант написання
 }
 type StudentType = {
