e4e0cd8a11d4d49733aafd132d7bc345573b24c9
Dinara <dinara198989@bk.ru>
2024-01-05 14:21:36 +0100

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./site/Header";
 import Body from "./site/Body";
 import Footer from "./site/Footer";
@@ -23,10 +23,10 @@ const App = () => {
   const foo2=(num:number)=> {
     console.log(num)
   }
-  const onClickHandler = (name:string) => {
-    console.log(name)
-  }
-
+  // const onClickHandler = (name:string) => {
+  //   console.log(name)
+  // }
+// Universal Button
   const Button1Foo=(subscriber:string, age:number, address:string)=>{
       console.log(subscriber, age, address)
   }
@@ -36,20 +36,43 @@ const App = () => {
   const Foo3 =()=>{
         console.log('Im stupid button')
   }
+  // Hook Use State
+   let[a, setA]=useState(1)
+    // a  is a state, setA is a function where a is existing
+   //variant with props name
+    const onClickHandler=(name:string)=>{
+      if (name === 'add') {
+          setA(++a);
+      }
+      if(name === 'zero') {
+           setA(0);
+      }
+      if(name === 'minus') {
+          setA(--a);
+      }
+   }
+
   return (
       <div>
-          <Button name={'Button 1'} callBack={()=>Button1Foo('Me Dinara', 35, 'from Ukraine')}/>
-          <Button name={'Button 1'} callBack={()=>Button2Foo('Me Hro', 43, 'from Lebanon')}/>
-          <Button name={'Stupid Button'} callBack={Foo3}/>
-          <Header title={'First'}/>
-          <Body titleBody={'Here is some content'}/>
-          {/*//here I practised map method*/}
-          <Component students={students}/>
-          <Footer titleFooter={'Here is some info about our company'}/>
-          <button onClick={foo1}>1</button>
-          <button onClick={() => foo2(10020)}>2</button>
-          <button onClick={(event) => onClickHandler('Dinara')}> My YouTube Chanel 1</button>
-          <button onClick={(event) => onClickHandler('Hro')}>My YouTube Chanel 2</button>
+          {/*Hook Use State*/}
+          <div>
+              <h2>{a}</h2>
+              <button onClick={()=>onClickHandler('add')}> + </button>
+              <button onClick={()=>onClickHandler('zero')}> 0 </button>
+              <button onClick={()=>onClickHandler('minus')}> - </button>
+          </div>
+          {/*<Button name={'Button 1'} callBack={()=>Button1Foo('Me Dinara', 35, 'from Ukraine')}/>*/}
+          {/*<Button name={'Button 1'} callBack={()=>Button2Foo('Me Hro', 43, 'from Lebanon')}/>*/}
+          {/*<Button name={'Stupid Button'} callBack={Foo3}/>*/}
+          {/*<Header title={'First'}/>*/}
+          {/*<Body titleBody={'Here is some content'}/>*/}
+          {/*//map method*/}
+          {/*<Component students={students}/>*/}
+          {/*<Footer titleFooter={'Here is some info about our company'}/>*/}
+          {/*<button onClick={foo1}>1</button>*/}
+          {/*<button onClick={() => foo2(10020)}>2</button>*/}
+          {/*<button onClick={(event) => onClickHandler('Dinara')}> My YouTube Chanel 1</button>*/}
+          {/*<button onClick={(event) => onClickHandler('Hro')}>My YouTube Chanel 2</button>*/}
 
       </div>
   );
